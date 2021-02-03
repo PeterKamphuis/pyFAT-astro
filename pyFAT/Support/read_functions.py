1c3865e671595f1f427c073afd533b5bbbbc0c23
PeterKamphuis <peterkamphuisastronomy@gmail.com>
2021-02-03 17:46:24 +0100

    Different systems

--- pyFAT/Support/read_functions.py
+++ pyFAT/Support/read_functions.py
@@ -536,7 +536,7 @@ def guess_orientation(Configuration,Fits_Files, center = None, debug = False):
                     min_RC_length = RC_length
                 if debug:
                     print_log(f'''GUESS_ORIENTATION: We found the lengths for the initial rotation curve:
-{'':8s}GUESS_ORIENTATION: RC length = {RC_length}, min_RC length = {RC_length}, Vrot ini = {len(VROT_initial)}, tmnp = {len(tmp)}
+{'':8s}GUESS_ORIENTATION: RC length = {RC_length}, min_RC length = {min_RC_length}, Vrot ini = {len(VROT_initial)}, tmnp = {len(tmp)}
 ''',Configuration['OUTPUTLOG'], debug = False, screen =True)
                 VROT_initial = np.vstack((VROT_initial[:min_RC_length],tmp[:min_RC_length]))
         VROT_initial = np.mean(VROT_initial,axis=0)
